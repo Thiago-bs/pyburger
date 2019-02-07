8ef37702b2b0689f5d0db7c85317c8c7949e9e74
Thiago <97thiagobs@gmail.com>
2019-02-07 09:29:42 -0200

    more 10% +ingredient

--- snackbar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SnackbarConfig(AppConfig):
-    name = 'snackbar'
--- snackbar/views.py
+++ snackbar/views.py
@@ -4,34 +4,37 @@ from snackbar.modelo import *
 # Create your views here.
 
 def defaultIngrients():
-    lettuce = Ingredient(0,"alface", 0.4, 1)
-    bacon = Ingredient(1,"Bacon", 2, 1)
-    burger = Ingredient(2,"Hambúrguer de carne", 3, 1)
-    egg = Ingredient(3,"Ovo", 0.8, 1)
-    cheese = Ingredient(4,"Queijo", 1.5, 1)
+    lettuce = Ingredient(0,"alface", 0.4, 0)
+    bacon = Ingredient(1,"Bacon", 2, 0)
+    burger = Ingredient(2,"Hambúrguer de carne", 3, 0)
+    egg = Ingredient(3,"Ovo", 0.8, 0)
+    cheese = Ingredient(4,"Queijo", 1.5, 0)
     ingredients = [lettuce, bacon, burger, egg, cheese]
     return ingredients
 
+def add_default_amount_ingredient(snack, ids_ingredients):
+    for ingredient in snack.ingredients_list:
+        if ingredient.id in ids_ingredients:
+            ingredient.amount = 1
+    return snack
+
 def getDefaultSnacks(ingredients):
-    bacon_ingrients = [bacon_ingrient for bacon_ingrient in ingredients if bacon_ingrient.id in [1,2,4] ]
-    x_bacon = Snacks(1,"X-Bacon",  "xbacon-img.jpg" , bacon_ingrients)
-    
-    burger_ingrients = [burger_ingrient for burger_ingrient in ingredients if burger_ingrient.id in [2,4]]
-    x_burger = Snacks(2,"X-Burger", "xburger-img.jpg", burger_ingrients )
-    
-    xegg_ingrients = [xegg_ingrient for xegg_ingrient in ingredients if xegg_ingrient.id in [3,2,4]]
-    x_egg = Snacks(3,"X-Egg","xegg-img.jpg",  xegg_ingrients)
 
-    xegg_bacon_ingrients = [xegg_bacon_ingrient for xegg_bacon_ingrient in ingredients if xegg_bacon_ingrient.id in [3,1, 2,4]]
-    x_egg_bacon = Snacks(4,"X-Egg Bacon", "xegg-bacon-img.jpg", xegg_bacon_ingrients )
+    x_bacon = Snacks(1,"X-Bacon",  "xbacon-img.jpg" , defaultIngrients())
+    x_bacon = add_default_amount_ingredient(x_bacon, [1,2,4])
+
+    x_burger = Snacks(2,"X-Burger", "xburger-img.jpg", defaultIngrients())
+    x_burger = add_default_amount_ingredient(x_burger, [2,4])
+
+    x_egg = Snacks(3,"X-Egg","xegg-img.jpg",  defaultIngrients())
+    x_egg = add_default_amount_ingredient(x_egg, [3,2,4])
+
+    x_egg_bacon = Snacks(4,"X-Egg Bacon", "xegg-bacon-img.jpg", defaultIngrients())
+    x_egg_bacon = add_default_amount_ingredient(x_egg_bacon, [3, 1, 2, 4])
+
     defaultSnacks = [x_bacon, x_burger, x_egg, x_egg_bacon]
-
     return defaultSnacks
 
-def count_ingredients(snack):
-    dict_ingredients = {}
-
-    return dict_ingredients
 
 def get_snack(id):
     snacks = getDefaultSnacks(defaultIngrients())
